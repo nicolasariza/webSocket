d1cdd8e18f72e2b68b2411d0c83d7b3a3a325662
nicolasariza <nicolas_ariza@outlook.com>
2021-06-19 00:38:40 -0500

    events added

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,1 +1,14 @@
+const labelOnline = document.querySelector('#labelOnline');
+const labelOffline = document.querySelector('#labelOffline');
+
 const socket = io();
+
+socket.on('connect', ()=> {
+    labelOnline.hidden = false;
+    labelOffline.hidden = true;
+});
+
+socket.on('disconnect', ()=> {
+    labelOnline.hidden = true;
+    labelOffline.hidden = false;
+});
